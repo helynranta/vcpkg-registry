a4c211a5fc1ed9782ab140c670686c6b346f5a84
Lassi Helynranta <lassi.helynranta@helen.fi>
2022-10-07 06:46:57 +0300

    updated

--- ports/hypodermic/portfile.cmake
+++ ports/hypodermic/portfile.cmake
@@ -17,13 +17,10 @@ set(HYPODERMIC_VERSION
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
-)
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 
