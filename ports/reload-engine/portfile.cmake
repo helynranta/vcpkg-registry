b67329ed438b716444bf4f74705fcbe0b65f7736
Lassi Helynranta <lassi@helynranta.com>
2022-10-18 22:28:21 +0300

    [engine] update to v0.0.148

--- ports/reload-engine/portfile.cmake
+++ ports/reload-engine/portfile.cmake
@@ -1,14 +1,14 @@
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 if (NOT DEFINED ENV{GITHUB_AUTHORIZATION_TOKEN})
-    message(FATAL_ERROR "environment variable GITHUB_AUTHORIZATION_TOKEN must be defined")
+    message(STATUS "Environment variable GITHUB_AUTHORIZATION_TOKEN is not defined, make sure you have environment that is capable to logging in by itself")
 endif()
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO lerppana/reload-engine
-    REF v0.0.145
-    SHA512 2051def1e7a534495afd0cb58d81234d00a089e79fe14310f85bf9a35278508758646e6277e7abcd130ebde5aa865c5ca7c714e7709b7137180a244c7270eb8b
+    REF 0c0d304694ececeddb87f2158870b8949da6adb8 # v0.0.148
+    SHA512 4638a3a74559fa5d2ac04db28f61fd002d19513bef0f85bfa9a5cb11a63622baa14b53ab7db955b9683162fb4b880697336205d53a2a97fded855568a68d7970
     HEAD_REF master
     AUTHORIZATION_TOKEN $ENV{GITHUB_AUTHORIZATION_TOKEN}
 )
@@ -27,6 +27,7 @@ vcpkg_cmake_configure(
 )
 
 file(INSTALL
+        "${SOURCE_PATH}/.cmake/copy_dll_to.cmake"
         "${SOURCE_PATH}/.cmake/date_to_version.cmake"
         "${SOURCE_PATH}/.cmake/engine_create.cmake"
         "${SOURCE_PATH}/.cmake/engine_create_launcher.cmake"
@@ -42,9 +43,12 @@ vcpkg_cmake_config_fixup(
         TOOLS_PATH ${CURRENT_PACKAGES_DIR}/tools/engine)
 
 vcpkg_copy_tools(TOOL_NAMES
-        launcher reflection resource_compiler resource_compressor asset0.data gltf2_export.py
+        launcher reflection resource_compiler resource_compressor
         DESTINATION ${CURRENT_PACKAGES_DIR}/tools/engine
         AUTO_CLEAN)
+
+file(RENAME "${CURRENT_PACKAGES_DIR}/bin/asset0.data" "${CURRENT_PACKAGES_DIR}/tools/engine/asset0.data")
+file(RENAME "${CURRENT_PACKAGES_DIR}/bin/gltf2_export.py" "${CURRENT_PACKAGES_DIR}/tools/engine/gltf2_export.py")
 
 vcpkg_add_to_path("${CURRENT_PACKAGES_DIR}/share/engine")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
