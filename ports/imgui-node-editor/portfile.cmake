1163e0e1598b058af6b8ceee96c45b9e5eebee35
Lassi Helynranta <lassi@helynranta.com>
2023-10-14 14:07:34 +0300

    [imgui-node-editor] update from upstream

--- ports/imgui-node-editor/portfile.cmake
+++ ports/imgui-node-editor/portfile.cmake
@@ -3,19 +3,22 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO helynranta/imgui-node-editor
-    REF 12becd312a7fd94f96551507ddc26153c09cd5a6
-    SHA512 259a1b5c77ab96b8f940ab43ba744a717c75b1f36fcbebabeb6975edbce21270304bab01dd9f571fe0906d4210d9eacb4af38300117b59c994c019b9cec4694f
-    HEAD_REF develop
+    REF af7fa51bb9d68c9b44477c341f13a7dadee3e359
+    SHA512 5ee9f39df550c8b9cac2cfb5ec048732f68ed3f13f0f93feeee655b531d0127d5c255081600971fe3dc1d6df3f00bdb46ecc8cd6fbe2078c802cd8537139a737
+    HEAD_REF master
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS_DEBUG
+        -DIMGUI_NODE_EDITOR_SKIP_HEADERS=ON
+)
+
 vcpkg_cmake_install()
+
 vcpkg_copy_pdbs()
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-${PORT} CONFIG_PATH share/unofficial-${PORT})
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
-
-# Put the license file where vcpkg expects it
-file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/imgui-node-editor/)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/imgui-node-editor/LICENSE ${CURRENT_PACKAGES_DIR}/share/imgui-node-editor/copyright)
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
